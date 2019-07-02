746b6675e9489f17c00ec3a8c989c35c22ed3128
unknown <mariano.la22@gmail.com>
2019-07-01 22:04:56 -0300

    Commit Mariano Mazzucca

---
+++ UrlShortener.Tests/Controllers/GetUrlTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Net;
+using System.Net.Http;
+
+namespace UrlShortener.Tests.Controllers
+{
+    [TestClass]
+    public class GetUrlTests : MainControllerTests
+    {
+        [TestMethod]
+        public void GetUrlTests_Success()
+        {
+            urlDataRepository.Setup(x => x.GetUrl(It.IsAny<string>())).Returns(new Uri("http://www.google.com/"));
+
+            var response = urlsController.GetUrl("Test12");
+            Assert.IsTrue(response is IActionResult);
+            Assert.IsTrue(response is RedirectResult);
+            Assert.IsTrue(((RedirectResult)response).Url.Equals("http://www.google.com/"));
+        }
+
+        [TestMethod]
+        public void GetUrlTests_Code_NotFound_Error()
+        {
+            urlDataRepository.Setup(x => x.GetUrl("Test12")).Returns(new Uri("http://www.google.com/"));
+
+            var response = urlsController.GetUrl("12Test");
+            Assert.IsTrue(response is IActionResult);
+            Assert.IsTrue(response is NotFoundResult);
+        }
+
+        [TestMethod]
+        public void GetUrlTests_EmptyCode_NotFound_Error()
+        {
+            var response = urlsController.GetUrl(String.Empty);
+            Assert.IsTrue(response is IActionResult);
+            Assert.IsTrue(response is NotFoundResult);
+        }
+    }
+}
