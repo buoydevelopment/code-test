6f818ec0a4e7ce33049e76d5851a1ed1ac7bd429
Mauro <mauro@sdf.org>
2019-10-19 21:52:01 -0300

    adding tests for each endpoint

--- test_shorturl.py
+++ test_shorturl.py
@@ -13,8 +13,48 @@ class ShorturlTests(unittest.TestCase):
         pass
 
     def test_index(self):
-        response = self.app.get('/', follow_redirects = True)
+        response = self.app.get('/',
+                                follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_url_get_ok(self):
+        response = self.app.get('/abc123',
+                                follow_redirects=False)
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_get_err(self):
+        response = self.app.get('/abc123',
+                                follow_redirects=False)
+        self.assertEqual(response.status_code, 302)
+
+    def test_url_stats(self):
+        response = self.app.get('/abc123/stats',
+                                follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_post_ok(self):
+        response = self.app.post('/urls',
+                                 data={
+                                     "url": "http://ddg.gg/",
+                                     "code": "ddg123"
+                                 })
+        self.assertEqual(response.status_code, 201)
+
+    def test_url_post_err(self):
+        response = self.app.post('/urls',
+                                 data={
+                                     "url": "http://ddg.gg/",
+                                     "code": "wololo"
+                                 })
+        self.assertEqual(response.status_code, 201)
+
+    def test_url_post_nocode(self):
+        response = self.app.post('/urls',
+                                 data={
+                                     "url": "http://sdf.org/",
+                                 })
+        self.assertEqual(response.status_code, 201)
+
 
 if __name__ == "__main__":
     unittest.main()
