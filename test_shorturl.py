e4f8c9a8c3f15e24956ac90019fda9d9d64fc147
Mauro <mauro@sdf.org>
2019-10-20 20:23:41 -0300

    UPDATE: added minor checks in test_url_stats

--- test_shorturl.py
+++ test_shorturl.py
@@ -1,7 +1,20 @@
 import json
 import utils
+import datetime
 import unittest
 from shorturl import app
+
+
+def cmp_dt(dt_a: str, dt_b: str) -> bool:
+    dt_a = datetime.datetime.strftime(
+        datetime.datetime.strptime(
+            dt_a, '%Y-%m-%dT%H:%M:%S.%f'
+        ), '%Y-%m-%dT%H:%M:%S'
+    )
+    dt_b = datetime.datetime.strftime(
+         dt_b, '%Y-%m-%dT%H:%M:%S'
+    )
+    return dt_a == dt_b
 
 
 class ShorturlTests(unittest.TestCase):
@@ -72,16 +85,6 @@ class ShorturlTests(unittest.TestCase):
                                  content_type='application/json')
         self.assertEqual(response.status_code, 422)
 
-
-    def test_url_post_422(self):
-        self.app.post('/urls',
-                      data=json.dumps(self.data),
-                      content_type='application/json')
-        response = self.app.post('/urls',
-                                 data=json.dumps(self.data),
-                                 content_type='application/json')
-        self.assertEqual(response.status_code, 422)
-
     def test_url_get_302(self):
         post = self.app.post('/urls',
                                  data=json.dumps({
@@ -99,8 +102,8 @@ class ShorturlTests(unittest.TestCase):
                                 follow_redirects=False)
         self.assertEqual(response.status_code, 404)
 
-
     def test_url_stats_200(self):
+        now = utils.now()
         post = self.app.post('/urls',
                                  data=json.dumps({
                                      "url": self.data["url"]
@@ -111,10 +114,17 @@ class ShorturlTests(unittest.TestCase):
         for i in range(usage_expected):
             self.app.get(f"/{code}",
                          follow_redirects=False)
+            now_usage = utils.now()
 
         response = self.app.get(f"/{code}/stats")
+
         self.assertEqual(response.json["usage_count"],
                          usage_expected)
+        self.assertTrue(cmp_dt(response.json["created_at"],
+                               now))
+        self.assertTrue(cmp_dt(response.json["last_usage"],
+                               now_usage))
+
 
     def test_url_stats_404(self):
         response = self.app.get("/err404/stats")
