11ec085a3ab658b20e02295d6cf42aa19811ea30
Ale Lopez <alelopez.utnfrt@gmail.com>
2020-05-30 14:10:24 -0300

    Added UrlsController#show action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,13 +1,25 @@
 class UrlsController < ApplicationController
+  before_action :set_url, only: [:show]
+
   def create
     url = Url.create(url_params)
 
     render json: {code: url.code}, status: :created
   end
 
+  def show
+    render json: @url, status: :found
+  end
+
   private
 
   def url_params
     params.permit(:url, :code)
+  end
+
+  def set_url
+    @url = Url.find_by!(code: params[:code])
+  rescue ActiveRecord::RecordNotFound
+    render status: :not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :urls, only: [:create]
+  resources :urls, only: [:create, :show], param: :code
 end
